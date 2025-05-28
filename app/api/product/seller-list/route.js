be79a3774eb2a1bd5a0f27e316094e4bee760f1c
Lan Anh <dhlananh2309@gmail.com>
2025-05-28 18:30:24 +0700

    build: update clerk package import

--- app/api/product/seller-list/route.js
+++ app/api/product/seller-list/route.js
@@ -1,7 +1,7 @@
 import connectDB from "@/config/db";
 import authSeller from "@/lib/authSeller";
 import Product from "@/models/Product";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
